bad1d9637867f481d064fcaeaa562eaae6889d93
Mark Lester Bolotaolo <mlab817@gmail.com>
2023-05-22 07:30:10 +0800

    Update pusher_client.dart
    working on websockets

--- lib/data/data_sources/pusher_client.dart
+++ lib/data/data_sources/pusher_client.dart
@@ -10,18 +10,47 @@ const hostOptions = PusherChannelsOptions.fromHost(
   key: '1b421e8d437e47b9eee3',
 // decide if to send additional metadata in query parameters of the url
   shouldSupplyMetadataQueries: true,
+  port: 443,
 // apply custom metadata if needed
   metadata: PusherChannelsOptionsMetadata.byDefault(),
 );
 
 final pusherClientProvider = Provider<PusherChannelsClient>((ref) {
+  print('pusher client init');
+
   final client = PusherChannelsClient.websocket(
-      options: hostOptions,
-      connectionErrorHandler: (exception, trace, refresh) {
-        refresh();
-      });
+    options: hostOptions,
+    connectionErrorHandler: (exception, trace, refresh) {
+      print(exception);
+      print(trace);
+      refresh();
+    },
+  );
+
+  final publicChannels = <Channel>[
+    client.publicChannel('PIPSv3.Public'),
+  ];
+
+  final subscriptions = client.onConnectionEstablished.listen((_) {
+    for (var channel in publicChannels) {
+      channel.subscribeIfNotUnsubscribed();
+    }
+  });
+
+  publicChannels.first.bind('pusher:subscription_succeeded').listen((_) {
+    print('subscription succeeded');
+  });
+
+  publicChannels.first.bindToAll().listen((event) {
+    // do something
+    print(event.data.toString());
+  });
 
   client.connect();
+
+  ref.onDispose(() {
+    client.dispose();
+  });
 
   return client;
 });
