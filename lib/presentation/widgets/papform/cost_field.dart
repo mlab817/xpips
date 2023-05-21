33fb4f8d51a7a8419f65648aaf6c6c939c014a9a
Mark Lester Bolotaolo <mlab817@gmail.com>
2023-05-22 07:32:04 +0800

    Update cost_field.dart
    new provider for inputDecoration and numeric text formatter

--- lib/presentation/widgets/papform/cost_field.dart
+++ lib/presentation/widgets/papform/cost_field.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:intl/intl.dart';
+import 'package:pips/application/providers/inputdecoration_provider.dart';
 
-class CostField extends StatelessWidget {
+class CostField extends ConsumerWidget {
   const CostField({Key? key, required this.onChanged, this.enabled, this.value})
       : super(key: key);
 
@@ -9,28 +13,43 @@ class CostField extends StatelessWidget {
   final String? value;
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return TextFormField(
       // controller: controller,
       initialValue: value ?? '',
-      decoration: _getTextInputDecoration(),
+      decoration: ref.watch(inputDecorationProvider),
       textAlign: TextAlign.right,
       textAlignVertical: TextAlignVertical.center,
       expands: false,
       enabled: enabled ?? true,
       onChanged: onChanged,
+      inputFormatters: [
+        NumericTextFormatter(),
+      ],
     );
   }
+}
 
-  InputDecoration _getTextInputDecoration() {
-    return const InputDecoration(
-      isCollapsed: true,
-      border: InputBorder.none,
-      focusedBorder: InputBorder.none,
-      enabledBorder: InputBorder.none,
-      disabledBorder: InputBorder.none,
-      fillColor: Colors.transparent,
-      hoverColor: Colors.transparent,
-    );
+class NumericTextFormatter extends TextInputFormatter {
+  @override
+  TextEditingValue formatEditUpdate(
+      TextEditingValue oldValue, TextEditingValue newValue) {
+    if (newValue.text.isEmpty) {
+      return newValue.copyWith(text: '');
+    } else if (newValue.text.compareTo(oldValue.text) != 0) {
+      final int selectionIndexFromTheRight =
+          newValue.text.length - newValue.selection.end;
+      final f = NumberFormat("#,###");
+      final number =
+          int.parse(newValue.text.replaceAll(f.symbols.GROUP_SEP, ''));
+      final newString = f.format(number);
+      return TextEditingValue(
+        text: newString,
+        selection: TextSelection.collapsed(
+            offset: newString.length - selectionIndexFromTheRight),
+      );
+    } else {
+      return newValue;
+    }
   }
 }
