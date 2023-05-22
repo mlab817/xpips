8b35b9b42df0168c41df1325810e3d59a40aa2dd
Mark Lester Bolotaolo <mlab817@gmail.com>
2023-05-23 07:22:11 +0800

    converted to autogenerated

--- lib/application/providers/sharedpreferences.dart
+++ lib/application/providers/sharedpreferences.dart
@@ -1,6 +1,8 @@
-import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
-final sharedPreferencesProvider = Provider<SharedPreferences>((ref) {
-  throw UnimplementedError();
-});
+part 'sharedpreferences.g.dart';
+
+@Riverpod(keepAlive: true)
+SharedPreferences sharedPreferences(SharedPreferencesRef ref) =>
+    throw UnimplementedError();
