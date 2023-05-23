855209f5333ac3e9ae4c15249eebf53154301f3d
Mark Lester Bolotaolo <mlab817@gmail.com>
2023-05-23 15:16:53 +0800

    Update offices_controller.dart
    reimplemented officefilter

--- lib/presentation/controllers/offices_controller.dart
+++ lib/presentation/controllers/offices_controller.dart
@@ -1,9 +1,11 @@
 import 'dart:async';
 
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:pips/data/responses/offices_response.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
 import '../../data/repositories/offices_repository.dart';
+import '../../domain/models/office.dart';
 
 part 'offices_controller.g.dart';
 
@@ -12,7 +14,20 @@ Future<OfficesResponse> offices(OfficesRef ref) {
   return ref.watch(officesRepositoryProvider).get();
 }
 
-final officeFilterProvider = Provider<String?>((ref) => '');
+final officeFilterProvider = StateProvider<String>((ref) => '');
+
+final filteredOfficesProvider = Provider<List<Office>>((ref) {
+  final offices = ref.watch(officesProvider).value;
+  final filterValue = ref.watch(officeFilterProvider).toLowerCase();
+
+  if (filterValue.isNotEmpty) {
+    return offices?.data.where((office) =>
+        office.name.toLowerCase().contains(filterValue.toLowerCase()) ||
+        office.acronym.toLowerCase().contains(filterValue.toLowerCase())).toList() ?? <Office>[];
+  }
+
+  return offices?.data ?? <Office>[];
+});
 
 // final filteredOfficesControllerProvider =
 //     Provider<AsyncValue<OfficesResponse>>((ref) {
