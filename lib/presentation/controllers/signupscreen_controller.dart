6f7f12d790114e36800c7f054c17109a849237a8
Mark Lester Bolotaolo <mlab817@gmail.com>
2023-05-23 15:20:43 +0800

    changed signuprequestcontroller to keepalive
    signuprequestcontroller is overwritten when authorization file is uploaded

--- lib/presentation/controllers/signupscreen_controller.dart
+++ lib/presentation/controllers/signupscreen_controller.dart
@@ -1,22 +1,29 @@
 import 'dart:async';
+import 'dart:io';
 
-import 'package:pips/data/repositories/auth_repository.dart';
+import 'package:flutter/foundation.dart';
 import 'package:pips/data/requests/signup_request.dart';
+import 'package:pips/data/responses/responses.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+import '../../data/repositories/repositories.dart';
 
 part 'signupscreen_controller.g.dart';
 
-@riverpod
+@Riverpod(keepAlive: true)
 class SignupRequestController extends _$SignupRequestController {
-  void update(
-      {String? username,
-      String? firstName,
-      String? lastName,
-      String? position,
-      String? email,
-      String? contactNumber,
-      int? officeId,
-      String? authorization}) {
+  void update({
+    String? username,
+    String? firstName,
+    String? lastName,
+    String? position,
+    String? email,
+    String? contactNumber,
+    int? officeId,
+    String? password,
+  }) {
+    final authorization =
+        ref.read(authorizationFileUploadControllerProvider).value;
     state = state.copyWith(
       username: username ?? state.username,
       firstName: firstName ?? state.firstName,
@@ -25,21 +32,14 @@ class SignupRequestController extends _$SignupRequestController {
       email: email ?? state.email,
       contactNumber: contactNumber ?? state.contactNumber,
       officeId: officeId ?? state.officeId,
-      authorization: authorization ?? state.authorization,
+      authorization: authorization?.path,
+      password: password ?? state.password,
     );
   }
 
   @override
   SignupRequest build() {
-    return SignupRequest(
-      username: '',
-      firstName: '',
-      lastName: '',
-      position: '',
-      email: '',
-      contactNumber: '',
-      officeId: null,
-    );
+    return SignupRequest.initial();
   }
 }
 
@@ -56,4 +56,35 @@ class SignupController extends _$SignupController {
 
   @override
   FutureOr<void> build() => null;
+}
+
+@Riverpod(keepAlive: true)
+class AuthorizationFileUploadController
+    extends _$AuthorizationFileUploadController {
+  Future<UploadResponse?> upload(File file) async {
+    print('AuthorizationFileUploadController triggered');
+
+    final repository = ref.watch(uploadRepositoryProvider);
+
+    state = const AsyncLoading();
+
+    state = await AsyncValue.guard(() => repository.upload(file));
+
+    return repository.upload(file);
+  }
+
+  Future<UploadResponse?> webUpload(Uint8List file) async {
+    print('AuthorizationFileUploadController triggered');
+
+    final repository = ref.watch(uploadRepositoryProvider);
+
+    state = const AsyncLoading();
+
+    state = await AsyncValue.guard(() => repository.webUpload(file));
+
+    return repository.webUpload(file);
+  }
+
+  @override
+  FutureOr<UploadResponse?> build() => Future.value(null);
 }
--- lib/presentation/controllers/signupscreen_controller.g.dart
+++ lib/presentation/controllers/signupscreen_controller.g.dart
@@ -7,12 +7,12 @@ part of 'signupscreen_controller.dart';
 // **************************************************************************
 
 String _$signupRequestControllerHash() =>
-    r'4d37b9dc8296ac3bc21136603e29f3486833df72';
+    r'16e6c66d027a2ceaca2f900454de4cdae1a64135';
 
 /// See also [SignupRequestController].
 @ProviderFor(SignupRequestController)
-final signupRequestControllerProvider = AutoDisposeNotifierProvider<
-    SignupRequestController, SignupRequest>.internal(
+final signupRequestControllerProvider =
+    NotifierProvider<SignupRequestController, SignupRequest>.internal(
   SignupRequestController.new,
   name: r'signupRequestControllerProvider',
   debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
@@ -22,7 +22,7 @@ final signupRequestControllerProvider = AutoDisposeNotifierProvider<
   allTransitiveDependencies: null,
 );
 
-typedef _$SignupRequestController = AutoDisposeNotifier<SignupRequest>;
+typedef _$SignupRequestController = Notifier<SignupRequest>;
 String _$signupControllerHash() => r'956848464a190a32d3bea6dc11f4a8875485505b';
 
 /// See also [SignupController].
@@ -39,4 +39,21 @@ final signupControllerProvider =
 );
 
 typedef _$SignupController = AutoDisposeAsyncNotifier<void>;
+String _$authorizationFileUploadControllerHash() =>
+    r'5a6f9ff17f162ee998274442a299f681d728969f';
+
+/// See also [AuthorizationFileUploadController].
+@ProviderFor(AuthorizationFileUploadController)
+final authorizationFileUploadControllerProvider = AsyncNotifierProvider<
+    AuthorizationFileUploadController, UploadResponse?>.internal(
+  AuthorizationFileUploadController.new,
+  name: r'authorizationFileUploadControllerProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$authorizationFileUploadControllerHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$AuthorizationFileUploadController = AsyncNotifier<UploadResponse?>;
 // ignore_for_file: unnecessary_raw_strings, subtype_of_sealed_class, invalid_use_of_internal_member, do_not_use_environment, prefer_const_constructors, public_member_api_docs, avoid_private_typedef_functions
