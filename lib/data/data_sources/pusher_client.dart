5436560cf4d6ba0191e6b07cfa9a60ba7acc1d42
Mark Lester Bolotaolo <mlab817@gmail.com>
2023-05-26 06:09:56 +0800

    replaced hostOptions with provider pattern

--- lib/data/data_sources/pusher_client.dart
+++ lib/data/data_sources/pusher_client.dart
@@ -1,51 +1,36 @@
 import 'package:dart_pusher_channels/dart_pusher_channels.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
 
 import '../../application/config.dart';
 
-const hostOptions = PusherChannelsOptions.fromHost(
+part 'pusher_client.g.dart';
+
+@Riverpod(keepAlive: true)
+PusherChannelsOptions pusherChannelsOptions(PusherChannelsOptionsRef ref) {
+  return const PusherChannelsOptions.fromHost(
 // usually: ws or wss
-  scheme: 'wss',
-  host: Config.domain,
-  key: '1b421e8d437e47b9eee3',
+    scheme: 'wss',
+    host: Config.domain,
+    key: '1b421e8d437e47b9eee3',
 // decide if to send additional metadata in query parameters of the url
-  shouldSupplyMetadataQueries: true,
-  port: 443,
+    shouldSupplyMetadataQueries: true,
+    port: 443,
 // apply custom metadata if needed
-  metadata: PusherChannelsOptionsMetadata.byDefault(),
-);
+    metadata: PusherChannelsOptionsMetadata.byDefault(),
+  );
+}
 
-final pusherClientProvider = Provider<PusherChannelsClient>((ref) {
-  print('pusher client init');
-
+@Riverpod(keepAlive: true)
+PusherChannelsClient pusherClient(PusherClientRef ref) {
   final client = PusherChannelsClient.websocket(
-    options: hostOptions,
+    options: ref.watch(pusherChannelsOptionsProvider),
     connectionErrorHandler: (exception, trace, refresh) {
       print(exception);
       print(trace);
       refresh();
     },
   );
-
-  // final publicChannels = <Channel>[
-  //   client.publicChannel('PIPSv3.Public'),
-  // ];
-  //
-  // final subscriptions = client.onConnectionEstablished.listen((_) {
-  //   for (var channel in publicChannels) {
-  //     channel.subscribeIfNotUnsubscribed();
-  //   }
-  // });
-  //
-  // publicChannels.first.bind('pusher:subscription_succeeded').listen((_) {
-  //   print('subscription succeeded');
-  // });
-  //
-  // publicChannels.first.bindToAll().listen((event) {
-  //   // do something
-  //   print('from pusherClientProvider bindToAll');
-  //   print(event.data.toString());
-  // });
 
   client.connect();
 
@@ -56,4 +41,4 @@ final pusherClientProvider = Provider<PusherChannelsClient>((ref) {
   });
 
   return client;
-});
+}
---
+++ lib/data/data_sources/pusher_client.g.dart
@@ -0,0 +1,39 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'pusher_client.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$pusherChannelsOptionsHash() =>
+    r'59aa1b324fb07c3f86b5f9eeb9e0f5f536cdef21';
+
+/// See also [pusherChannelsOptions].
+@ProviderFor(pusherChannelsOptions)
+final pusherChannelsOptionsProvider = Provider<PusherChannelsOptions>.internal(
+  pusherChannelsOptions,
+  name: r'pusherChannelsOptionsProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$pusherChannelsOptionsHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef PusherChannelsOptionsRef = ProviderRef<PusherChannelsOptions>;
+String _$pusherClientHash() => r'1f1eef9e61dbd22d5c2e866d03b183ec8a8a6054';
+
+/// See also [pusherClient].
+@ProviderFor(pusherClient)
+final pusherClientProvider = Provider<PusherChannelsClient>.internal(
+  pusherClient,
+  name: r'pusherClientProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$pusherClientHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef PusherClientRef = ProviderRef<PusherChannelsClient>;
+// ignore_for_file: unnecessary_raw_strings, subtype_of_sealed_class, invalid_use_of_internal_member, do_not_use_environment, prefer_const_constructors, public_member_api_docs, avoid_private_typedef_functions
