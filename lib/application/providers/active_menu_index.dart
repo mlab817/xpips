74205c24f05ac6c07d4c9dc32cd96f866e48d871
Mark Lester Bolotaolo <mlab817@gmail.com>
2023-06-30 11:38:38 +0800

    remove unused files

--- lib/application/providers/active_menu_index.dart
+++
@@ -1,3 +0,0 @@
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-
-final activeMenuIndexProvider = StateProvider((ref) => 0);
--- lib/application/providers/scroller_provider.dart
+++
@@ -1,5 +0,0 @@
-import 'package:flutter/cupertino.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-
-final scrollControllerProvider =
-    Provider<ScrollController>((ref) => ScrollController());
